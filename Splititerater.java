be0bb72a7c0f22b986117b79db762195c4d66dc6
RavindraReddy4G5 <73584109+RavindraReddy4G5@users.noreply.github.com>
2020-11-02 20:32:05 +0530

    Add files via upload

---
+++ Splititerater.java
@@ -0,0 +1,31 @@
+package com.jnit;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Splititerater {
+  public static void main(String a[]){
+    ListIterator<String> listIterator = null;
+    List<String> name = new ArrayList<String>();
+	List<String> place = new ArrayList<String>();
+	List<String> bike = new ArrayList<String>();
+	List<String> area = new ArrayList<String>();
+    name.add("Ravindra");
+    place.add("Hyderabad");
+    bike.add("MT15");
+    area.add("goa");
+    listIterator=name.listIterator();
+    listIterator=place.listIterator();
+    listIterator=bike.listIterator();
+    listIterator=area.listIterator(); 
+    System.out.println("Traversing the list in forward direction:");
+    while(listIterator.hasNext()){
+       System.out.println(listIterator.next());
+    }
+    System.out.println("\nTraversing the list in backward direction:");
+    while(listIterator.hasPrevious()){
+       System.out.println(listIterator.previous());
+    }
+  }
+}

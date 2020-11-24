4d4aec8b6e2db86434f9bc71fcf43ecd70ee1d3e
RavindraReddy4G5 <73584109+RavindraReddy4G5@users.noreply.github.com>
2020-11-24 19:02:13 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,24 +1,21 @@
-package com.jnit.Day17_11_2020;
+package com.jnit.JDBC.Day24_11_2020;
 
-import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-public class Student implements Serializable{
-	int Id;
-	 String name;
-	 double marks;
-
-public Student(int id, String name, double marks) {
-		super();
-		Id = id;
-		this.name = name;
-		this.marks = marks;
+public class Student {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		String url="jdbc:mysql://localhost:1432/motivity";
+		String usename="root";
+		String password="root";
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection connection =DriverManager.getConnection(url, usename, password);
+		Statement statement =connection.createStatement();
+		int x=statement.executeUpdate("insert into employee values(1006,235000,'pavan','amazon')");
+		
+		
 	}
-   public void Display()
-   {
-   	System.out.println("Id is :"+ this.Id);
-   	System.out.println(("Name is :"+this.name));
-   	System.out.println("Marks are :"+this.marks);
-   }
-
 
 }

590e6515c0bdcbeae5e19808c804e925d1544224
RavindraReddy4G5 <73584109+RavindraReddy4G5@users.noreply.github.com>
2020-12-16 18:20:19 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,21 +1,33 @@
-package com.jnit.JDBC.Day24_11_2020;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+package com.hybernate;
 
 public class Student {
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		String url="jdbc:mysql://localhost:1432/motivity";
-		String usename="root";
-		String password="root";
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		Connection connection =DriverManager.getConnection(url, usename, password);
-		Statement statement =connection.createStatement();
-		int x=statement.executeUpdate("insert into employee values(1006,235000,'pavan','amazon')");
-		
-		
+	private int id;
+	private String name;
+	private String marks;
+	private String age;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMarks() {
+		return marks;
+	}
+	public void setMarks(String marks) {
+		this.marks = marks;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
 	}
 
 }
